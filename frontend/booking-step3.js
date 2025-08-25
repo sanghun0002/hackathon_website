f3f768403237871f00e8e70acc616c3e93be93eb
monga18 <gahyun7437@naver.com>
2025-08-26 02:02:28 +0900

    monga

--- frontend/booking-step3.js
+++ frontend/booking-step3.js
@@ -15,13 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
     "삼계리 계곡": {
       image_url: "/images/map_samgyeri.png",
       sections: [
-            { name: "삼계리 1구역", top: "15%", left: "75%", width: "5%", height: "5%" },
-            { name: "삼계리 2구역", top: "20%", left: "70%", width: "5%", height: "5%" },
+            { name: "삼계리 1구역", top: "13%", left: "75%", width: "5%", height: "5%" },
+            { name: "삼계리 2구역", top: "20%", left: "72%", width: "5%", height: "5%" },
             { name: "삼계리 3구역", top: "18%", left: "57%", width: "5%", height: "5%" },
             { name: "삼계리 4구역", top: "28%", left: "53%", width: "5%", height: "5%" },
             { name: "삼계리 5구역", top: "40%", left: "50%", width: "5%", height: "5%" },
-            { name: "삼계리 6구역", top: "80%", left: "25%", width: "5%", height: "5%" },
-            { name: "삼계리 7구역", top: "87%", left: "15%", width: "5%", height: "5%" }
+            { name: "삼계리 6구역", top: "78%", left: "23%", width: "5%", height: "5%" },
+            { name: "삼계리 7구역", top: "87%", left: "13%", width: "5%", height: "5%" }
       ]
     }
   };
