0e08a31e2a3d110823c10e16c2bda63a4180169a
monga18 <gahyun7437@naver.com>
2025-08-26 18:32:10 +0900

    monga

--- frontend/booking-step4.js
+++ frontend/booking-step4.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
       decks: [
         { id: "1-1", name: "평상 1", top: "60%", left: "42%", width: "6%", height: "6%", capacity: 4, isBooked: false },
         { id: "1-2", name: "평상 2", top: "65%", left: "55%", width: "6%", height: "6%", capacity: 4, isBooked: true },
-        { id: "1-3", name: "평상 3", top: "67%", left: "67%", width: "6%", height: "6%", capacity: 6, isBooked: false }
+        { id: "1-3", name: "평상 3", top: "65%", left: "66%", width: "6%", height: "6%", capacity: 6, isBooked: false }
       ]
     },
     "삼계리 2구역": {
