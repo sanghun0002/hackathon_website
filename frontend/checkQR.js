025feb5050798b603fdb4b2392a373bc61d836bd
Jisung-Park-0609 <gs7dog@gmail.com>
2025-08-27 12:57:03 +0900

    Update checkQR.js

--- frontend/checkQR.js
+++ frontend/checkQR.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const resultMessage = document.getElementById('result-message');
     
     // --- 💻 백엔드 서버 주소를 여기에 설정 ---
-    const backendUrl = 'https://image-analyzer-wduj.onrender.com/verify-booking';
+    const backendUrl = 'https://65a8b868fc3c.ngrok-free.app/verify-booking';
 
     // 화면에 평상 ID 표시
     if (pyeongsangId) {
